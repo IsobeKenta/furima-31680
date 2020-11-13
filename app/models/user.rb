f4291722741ff722fdbe3dbcb69b9e2a78994a76
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-13 19:33:17 +0900

    正規表現の変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
   validates :birthday, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用してください' }
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  validates :family_name, presence: true, format: {with: NAME_REGEX, message: '全角文字を使用してください' }
+  validates :first_name, presence: true, format: { with: NAME_REGEX, message: '全角文字を使用してください' }
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :family_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: 'カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: NAME_KANA_REGEX, message: 'カタカナを使用してください' }
 end
