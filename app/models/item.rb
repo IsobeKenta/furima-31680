eb7d80a3c61e749898a24f8201701a5e030652d8
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-16 15:45:45 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status_id
   belongs_to_active_hash :delivaly_price
   belongs_to_active_hash :ship
-  validates :image, presence: true
-  validates :product, :explanation, presence: true
+  validates :image, :product, :explanation, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :type_id, :status_id_id, :delivaly_price_id, :prefecture_id, :ship_id, numericality: { other_than: 0 }
 end
