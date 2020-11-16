32d4249afc6ddfa1c80006d9d84181665ecd407b
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-16 12:40:55 +0900

    モデルの単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status_id
   belongs_to_active_hash :delivaly_price
   belongs_to_active_hash :ship
-
-  validates :product, :explanation, :image, presence: true
+  validates :image, presence: true
+  validates :product, :explanation, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :type_id, :status_id_id, :delivaly_price_id, :prefecture_id, :ship_id, numericality: { other_than: 0 }
+  
 end
