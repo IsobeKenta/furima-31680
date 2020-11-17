6bbb00d4d0b5a114c0f995a35104db974af6ea82
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-17 18:10:58 +0900

    if文の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
+    if user_signed_in? && current_user.id == @item.user_id 
+      if @item.destroy
+        redirect_to root_path
+     end
     end
   end
 
