65ebbc62cc3aac0a1825f16923a5ea174119603a
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-19 18:32:20 +0900

    モデル単体テストコード1

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,8 +7,8 @@ class OrderAddress
 
   with_options presence: true do
     validates :postel, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, format: {with:/\A[ぁ-んァ-ン一-龥]/}
-    validates :tel, format: {with:/\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/}
+    validates :city
+    validates :tel, format: {with: /\A\d{0,11}\z/}
     validates :house_number
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
+  describe '商品購入機能' do
+    context '商品購入がうまくいくとき' do
+      it '全ての項目の入力が存在すれば登録できる' do
+        expect(@order_address).to be_valid
+      end
+    end
+    context '商品購入がうまくいかないとき' do
+      it 'postelが空だと登録できない' do
+        @order_address.postel = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postel can't be blank")
+      end
+      it 'postelにハイフンがないと登録できない' do
+        @order_address.postel = 0000000
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postel is invalid. Include hyphen(-)")
+      end
+      it 'prefectureが指定されていないと登録できない' do
+        @order_address.prefecture_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'cityが空だと登録できない' do
+        @order_address.city = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'house_numberが空だと登録できない' do
+        @order_address.house_number = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("House number can't be blank")
+      end
+      it 'telが空だと登録できない' do
+        @order_address.tel = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Tel can't be blank")
+      end
+      it 'telにハイフンが入っていると登録できない' do
+        @order_address.tel = "000-000-000"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Tel is invalid")
+      end
+      it 'telが11桁以上だと登録できない' do
+        @order_address.tel = "000000000000"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Tel is invalid")
+      end
+    end
+  end
+end
