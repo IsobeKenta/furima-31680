57df04c161a714e60fc5c5cd6533e87e58e7c85e
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-20 19:56:48 +0900

    クレジット機能の記述をメソッド化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,12 +12,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -34,4 +29,12 @@ class OrdersController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+ end
 end
