c898ece912973665b26f751a255982426ac3b9fc
IsobeKenta <73214657+IsobeKenta@users.noreply.github.com>
2020-11-20 18:33:41 +0900

    ログイン状況、出品状況で表示を変える処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
+    if @item.user_id == current_user.id || @item.orders.present?
+      redirect_to root_path
+    end
   end
 
   def create
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-      if @order_address.valid?
-        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-        Payjp::Charge.create(
-          amount: @item.price,
-          card: order_params[:token],
-          currency: 'jpy'
-        )
-        @order_address.save
-        redirect_to root_path
-      else
-        render action: :index
-      end
+    if @order_address.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
